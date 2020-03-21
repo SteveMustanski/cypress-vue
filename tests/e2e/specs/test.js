71f655b40abc24264ae817a1df37bdfc615e9adc
SteveMustanski <steve.mustanski@gmail.com>
2020-03-21 13:11:50 -0400

    rename and fix test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,10 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('My First Test', () => {
-  it('Visits the app root url', () => {
+  beforeEach( () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+  })
+  it('Visits the app root url', () => {
+    cy.contains('h1', 'Welcome to the cypress test')
   })
 })
