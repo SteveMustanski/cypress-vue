edccb03bf82e5db765fc3fd96f4f38c9be89a3e2
SteveMustanski <steve.mustanski@gmail.com>
2020-03-21 13:33:59 -0400

    added tests for home and about pages

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/" data-cy='homeLink'>Home</router-link> |
+      <router-link to="/about" data-cy='aboutLink'>About</router-link>
     </div>
     <router-view/>
   </div>
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,10 +1,33 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
+describe('Home view', () => {
   beforeEach( () => {
     cy.visit('/')
   })
   it('Visits the app root url', () => {
+    cy.contains('h1', 'Welcome to the cypress test')
+  })
+  it('Has 4 plugin links', () => {
+    cy.get('[data-cy=plugins]').find('li').should('have.length', 4)
+  })
+  it('Has 5 essential links', () => {
+    cy.get('[data-cy=essential]').find('li').should('have.length', 5)
+  })
+  it('links to about page correctly', () => {
+    cy.get('[data-cy=aboutLink]').click()
+    cy.contains('h1', 'This is an about page')
+  })
+})
+
+describe('About view', () => {
+  beforeEach( () => {
+    cy.visit('/about')
+  })
+  it('displays the about info', () => {
+    cy.contains('h1', 'This is an about page')
+  })
+  it('links to the home page corretly', () => {
+    cy.get('[data-cy=homeLink]').click()
     cy.contains('h1', 'Welcome to the cypress test')
   })
 })
